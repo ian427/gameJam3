c4d269af8464e35fffbaf627b15b96560fdf4b45
ThomasDoylee <s224354@uos.ac.uk>
2024-02-27 12:31:00 +0000

    bobproto 1

--- Assets/Scripst/Effects/ButtonBob.cs
+++ Assets/Scripst/Effects/ButtonBob.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonBob : MonoBehaviour
 {
+    private Button ThisButton;
+    private RectTransform rectTransform;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rectTransform = GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        OnPointerEnter();
     }
-}
+    public void OnPointerEnter()
+    {
+
+        //  rectTransform.transform.localScale = Mathf.PingPong(new Vector3 (0,0,0), new Vector3(0, 0, 0));
+        gameObject.transform.localScale = gameObject.transform.localScale += (1, 1, 1);
+        Debug.Log("bounce");
+    }
+
+    }
+//https://discussions.unity.com/t/4-6-ui-how-to-detect-mouse-over-on-button/115607
+//https://docs.unity3d.com/ScriptReference/Mathf.PingPong.html
